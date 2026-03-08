688f58f60fab028da8eceeceb2997a035bc0eee0
Bby Lion <codelab042@gmail.com>
2026-03-08 12:27:33 +0100

    first commit

---
+++ app/user-dashboard/dashboard/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-gray-800">Welcome to Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
